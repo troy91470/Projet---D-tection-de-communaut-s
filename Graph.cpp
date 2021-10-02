608e77469aa7d4182b38750dd3843a9f99b15209
Alexandre-KHELIFI <83087888+Alexandre-KHELIFI@users.noreply.github.com>
2021-10-02 17:30:57 +0200

    Add files via upload
    better

--- Graph.cpp
+++ Graph.cpp
@@ -16,20 +16,20 @@ void Graph::printDab() {
 }
 
 void Graph::addVertex() {
-    std::cout << "dab\n";
+    std::cout << "V added\n";
+
 }
 
 void Graph::addEdge() {
-    std::cout << "dab\n";
+    std::cout << "EDGE added\n";
 }
 
 void Graph::deleteEdge() {
-    std::cout << "dab\n";
+    std::cout << "EDGE deleted\n";
 }
 
 int main() {
     Graph* g = new Graph();
     g->printDab();
-    std::cout << "prout";
     return 0;
 }
