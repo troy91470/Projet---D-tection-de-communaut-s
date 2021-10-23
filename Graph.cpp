307b68bdf6cc9cae170770c58f742696279cdd81
troy91470 <51080234+troy91470@users.noreply.github.com>
2021-10-23 17:29:09 +0200

    Add files via upload

--- Graph.cpp
+++ Graph.cpp
@@ -2,45 +2,54 @@
 
 Graph::Graph()
 {
-    countVertices=0;
-    adjacencyList={};
-    //ctor
+	countVertices=0;
+	adjacencyList={};
+	//ctor
 }
 
 Graph::~Graph()
 {
-    //dtor
+	//dtor
 }
 
 void Graph::printDab() {
-    for (int i=0;i<countVertices;i++) {
-        int siz = adjacencyList[i].size();
-        for (int j=0;j<siz;j++) {
-            cout << adjacencyList[i][j];
-        }
-        cout << "\n";
-    }
+	for (int i=0;i<countVertices;i++) {
+		int siz = adjacencyList[i].size();
+
+		cout << i << ": ";
+		for (int j=0;j<siz;j++) {
+            		cout << adjacencyList[i][j] << " ";	
+       	 	}
+        	cout << "\n";
+    	}
 }
 
 void Graph::addVertex(int num=1) {
-    for (int i=0;i<num;i++) {
-        countVertices++;
-        vector<int> newL;
-        adjacencyList.push_back(newL);
-        cout << "V added\n";
-    }
+	for (int i=0;i<num;i++) {
+        	countVertices++;
+       	 	vector<int> newL;
+		adjacencyList.push_back(newL);
+       		cout << "V added\n";
+	}
 }
 
 void Graph::addEdge(int v1, int v2) {
-    adjacencyList[v1].push_back(v2);
-    adjacencyList[v2].push_back(v1);
-    cout << "EDGE added\n";
+	adjacencyList[v1].push_back(v2);
+    	adjacencyList[v2].push_back(v1);
+    	cout << "EDGE added\n";
 }
 
 int main() {
-    Graph* g = new Graph();
-    g->addVertex(2);
-    g->addEdge(0,1);
-    g->printDab();
-    return 0;
+	Graph* g = new Graph();
+    	g->addVertex(3);
+    	g->addEdge(0,1);
+    	g->addEdge(0,2);
+    	g->addEdge(2,3);
+    	g->addEdge(1,2);
+    	g->printDab();
+    	return 0;
 }
+
+
+
+
--- Graph.h
+++ Graph.h
@@ -7,18 +7,18 @@ using namespace std;
 
 class Graph
 {
-    public:
-        Graph();
-        virtual ~Graph();
-        void printDab();
-        void addVertex(int num);
-        void addEdge(int v1, int v2);
+	public:
+		Graph();
+		virtual ~Graph();
+        	void printDab();
+        	void addVertex(int num);
+        	void addEdge(int v1, int v2);
 
-    protected:
+    	protected:
 
-    private:
-        int countVertices;
-        vector<vector<int>> adjacencyList;
+    	private:
+        	int countVertices;
+        	vector<vector<int>> adjacencyList;
 };
 
 #endif // GRAPH_H
