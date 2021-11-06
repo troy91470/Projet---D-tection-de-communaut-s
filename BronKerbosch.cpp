e56ee5e66e67d673dbdb10bdcb3fb9023acb7d0a
JonathanColin <62570200+JonathanColin@users.noreply.github.com>
2021-11-06 17:47:38 +0100

    Create BronKerbosch.cpp

---
+++ BronKerbosch.cpp
@@ -0,0 +1,321 @@
+#include <algorithm>
+#include <iterator>
+#include <map>
+#include <typeinfo>
+#include "Graph.cpp"
+
+using namespace std;
+
+
+
+vector<int> BronKerbosch(vector<int> &P, vector<int> &R, vector<int> &X, Graph G){
+    
+	if(P.size() + X.size() == 0){
+		return R;
+	}
+
+	for(int i = 0; i < P.size(); i){
+		
+		vector<int> interPVoisinsV;
+		vector<int> unionRetV;
+		vector<int> interXVoisinsV;
+		vector<int> ensembleV = {P[i]};
+		vector<int> voisinsV = graph->GetVoisins(P[i]);
+
+
+		/*std::set_intersection(P.begin(), P.end(),
+                          voisinsV.begin(), voisinsV.end(),
+                          std::back_inserter(interPVoisinsV));*/
+		intersection(P,voisinsV,interPVoisinsV);
+
+		std::set_union(R.begin(), R.end(),
+                          ensembleV.begin(), ensembleV.end(),
+                          std::back_inserter(unionRetV));
+
+		/*std::set_intersection(X.begin(), X.end(),
+                          voisinsV.begin(), voisinsV.end(),
+                          std::back_inserter(interXVoisinsV));*/
+		intersection(X,voisinsV,interXVoisinsV);
+
+		BronKerbosch(interPVoisinsV, unionRetV, interXVoisinsV,G);
+		X.push_back(P[i]);
+		P.erase(P.begin());
+
+	}
+	
+}
+
+
+
+vector<int> BronKerboschDegeneracy(Graph G){
+    
+	int positionSommetOrdreDege[G->GetNbSommets()];
+	vector<int> vectOrdreDegenerescence;
+	Graph* copieGraphe = Graph->CreerCopie();
+	ordreDegenerescence(copie_graphe,vectOrdreDegenerescence,positionSommetOrdreDege);
+
+	vector<int> P;
+	vector<int> R;
+	vector<int> X;
+
+	//int i = 0; i < G->GetNbSommets(); i++
+	for(int v : vectOrdreDegenerescence){
+
+
+
+
+		int posVDansOrdre = positionSommetOrdreDege[v];
+		vector<int> voisinsV = graph->GetVoisins(v);
+
+		for(int i = 0; i < voisinsV.size(); i++){
+			if(positionSommetOrdreDege[voisinsV[i]] > posVDansOrdre){
+    				P.push_back(voisinsV[i]);
+  			}
+			else{
+    				X.push_back(voisinsV[i]);
+			}
+		}
+
+		vector<int> ensembleV = {v};
+
+		BronKerboschPivot(P, ensembleV, X,G);
+	
+	}
+	
+}
+
+
+
+vector<int> BronKerboschPivot(vector<int> &P, vector<int> &R, vector<int> &X, Graph G){
+    
+	if(P.size() + X.size() == 0){
+		return R;
+	}
+
+
+	//Choose Pivot maximise P inter voisins u dans P U X
+	vector<int> unionPetX;
+	std::set_union(P.begin(), P.end(),
+                          X.begin(), X.end(),
+                          std::back_inserter(unionPetX));
+	vector<int> interPVoisinsU;
+	vector<int> voisinsU;
+	int maxPinterVoisinsU = 0;
+	int pivot = -1;
+	for(int u : unionPetX){
+		voisinsU = graph->GetVoisins(u);
+		/*std::set_intersection(P.begin(), P.end(),
+                          voisinsU.begin(), voisinsU.end(),
+                          std::back_inserter(interPVoisinsU));*/
+		intersection(P,voisinsU,interPVoisinsU);
+
+		if(interPVoisinsU.size() > maxPinterVoisinsU){
+			maxPinterVoisinsU = interPVoisinsU.size();
+			pivot = u;
+		}
+	}
+
+	voisinsPivot = graph->GetVoisins(pivot);
+	vector<int> PPriveVoisinsPivot;
+	v1PriveDeV2(P,voisinsPivot,PPriveVoisinsPivot);
+
+
+	for(int v : PPriveVoisinsPivot){
+		
+		vector<int> interPVoisinsV;
+		vector<int> unionRetV;
+		vector<int> interXVoisinsV;
+		vector<int> ensembleV = {v};
+		vector<int> voisinsV = graph->GetVoisins(v);
+
+
+		/*std::set_intersection(P.begin(), P.end(),
+                          voisinsV.begin(), voisinsV.end(),
+                          std::back_inserter(interPVoisinsV));*/
+		intersection(P,voisinsV,interPVoisinsV);
+
+		std::set_union(R.begin(), R.end(),
+                          ensembleV.begin(), ensembleV.end(),
+                          std::back_inserter(unionRetV));
+
+		/*std::set_intersection(X.begin(), X.end(),
+                          voisinsV.begin(), voisinsV.end(),
+                          std::back_inserter(interXVoisinsV));*/
+		intersection(X,voisinsV,interXVoisinsV);
+
+		BronKerboschPivot(interPVoisinsV, unionRetV, interXVoisinsV,G);
+	
+		P.erase(std::remove(P.begin(), P.end(), v), P.end());
+		X.push_back(v);
+		
+		
+		
+	}
+	
+}
+
+
+void ordreDegenerescence(Graph graphe, vector<int> &tabOrdreDege, int posSommetOrdreDege[]){
+	//get max degre graphe
+	int maxDeg = 0;
+	int degSommet;
+	for(int v : graphe->GetListeSommets()){
+		degSommet = graphe->GetDegres(v);
+		if(degSommet > maxDeg){
+			maxDeg = degSommet;
+		}
+	}
+
+	//bin sort
+	vector<int> tabDegres[maxDeg+1];
+	for(int v : graphe->GetListeSommets()){
+		degSommet = graphe->GetDegres(v);
+		tabDegres[degSommet].push_back(v);
+	}
+
+	int k = 0;
+	int l;
+	//int ordreDegenerescence[graphe->GetNbSommets()];
+
+	//commencement de l'ordre
+	while(graphe->GetNbSommets() > 0){
+
+		for(l=0; l < maxDeg+1; l++ ){
+			if(tabDegres[l].size() != 0){
+				break;
+			}	
+		}
+
+		posSommetOrdreDege[tabDegres[l][0]] = k;
+		tabOrdreDege.push_back(tabDegres[l][0]);
+		vector<int> voisinsSommetASup = graphe->GetVoisins(tabDegres[l][0]);
+		int degreVoisin;
+		for(int v : voisinsSommetASup){
+			degreVoisin = graphe->GetDegres(v);		
+			tabDegres[degreVoisin].erase(std::remove(tabDegres[degreVoisin].begin(), tabDegres[degreVoisin].end(), v), tabDegres[degreVoisin].end());
+			tabDegres[degreVoisin-1].push_back(v);
+		}
+		graphe->SupprimerSommet(tabDegres[l][0]);
+		graphe->RafraichirAretes();
+		k = k + 1;
+
+	}
+
+	
+	
+
+
+}
+
+
+template<typename Map>
+void intersection(vector<int> v1, vector<int> v2, vector<int> &vectorIntersection){
+
+	map<int, int> mapInter;
+
+	for(int v: v1){
+		mapInter[v] = 1;
+	}
+
+	for(int v: v2){
+		if(mapInter[v]){
+			mapInter[v] += 1;
+		}
+		else{
+			mapInter[v] = 1;
+		}
+	}
+
+	for(auto item : mapInter){
+		if(item.second == 2){
+			vectorIntersection.push_back(item.first);
+		}
+
+	}
+
+}
+
+void v1PriveDeV2(vector<int> v1, vector<int> v2, vector<int> &vectorV1PriveDeV2){
+
+	map<int, int> mapInter;
+
+	for(int v: v1){
+		mapInter[v] = 1;
+	}
+
+	for(int v: v2){
+		if(mapInter[v]){
+			mapInter[v] += 1;
+		}
+	}
+
+	for(auto item : mapInter){
+		if(item.second == 1){
+			vectorV1PriveDeV2.push_back(item.first);
+		}
+
+	}
+
+}
+
+int main(){
+
+
+	vector<int> nums;
+	nums.push_back(1);
+	nums.push_back(3);	
+	nums.push_back(4);
+	nums.push_back(5);
+	nums.push_back(9);
+
+	//std::sort(nums.begin(), nums.end());	
+
+	vector<int> num1;
+	
+	num1.push_back(2);
+	num1.push_back(7);
+	num1.push_back(8);
+	num1.push_back(10);
+
+/*	
+	vector<int> vectorRes;
+	//intersection(nums, num1, vectorRes);
+	v1PriveDeV2(nums,num1,vectorRes);
+
+	for(int i = 0; i < nums.size(); i){
+		cout << nums[i];
+		num1.push_back(nums[i]);
+		cout << nums[i] << "\n";
+		nums.erase(nums.begin());
+		
+		
+
+	}
+
+	//std::sort(num1.begin(), num1.end());
+	cout << "ens1: ";
+	for (int i = 0; i < nums.size(); i++) {
+		cout << nums[i] << ' ';
+	}
+
+	cout << "\n";
+	cout << "ens2: ";
+	for (int i = 0; i < num1.size(); i++) {
+		cout << num1[i] << ' ';
+	}
+
+	//BronKerbosch(num1,num1,nums);
+
+	cout << "\n";
+	cout << "prive: ";
+	for (int i = 0; i < vectorRes.size(); i++) {
+		cout << vectorRes[i] << " ";
+	}
+
+
+	*/
+	
+	
+
+	return 0;
+}
