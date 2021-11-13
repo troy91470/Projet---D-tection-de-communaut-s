4aca6c60e48621fa0d7485514b150c0e0a6c6c50
troy91470 <51080234+troy91470@users.noreply.github.com>
2021-11-13 19:12:26 +0100

    encore les commentaires gngnngnnnn

--- enumerationCliquesMaximales.h
+++ enumerationCliquesMaximales.h
@@ -1,5 +1,18 @@
-
+/*
+Renvoie le vecteur contenant toutes les cliques maximales du graphe donne en entree
+Algorithme n°1 du papier "A new decomposition technique for maximal clique enumeration for sparse graphs"
+*/
 vector<vector<int>> listeCliquesEnumAlgo1(Graph &graph);
+/*
+Renvoie le vecteur contenant toutes les cliques maximales du graphe donne en entree
+Algorithme n°2 du papier "A new decomposition technique for maximal clique enumeration for sparse graphs"
+*/
 vector<vector<int>> listeCliquesEnumAlgo2(Graph &graph);
+/*
+Renvoie un booleen selon si la clique est acceptable ou non
+*/
 bool verifClique(Graph* graph,vector<int> K,int ordreDegenerescence[],int sommetSousGraphe,int sommetEtude);
+/**
+Renvoie une copie du vecteur donne en entree, triee selon l'ordre donne en entree
+*/
 vector<int> trierVecteurSelonOrdre(vector<int> v,int* ordre,int taille);
